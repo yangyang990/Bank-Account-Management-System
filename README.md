44262ea2c5c6832cb5e112b9ff566abe2f0b11f2
nbzy05@gmail.com <nbzy05@gmail.com>
2025-08-25 14:48:48 +0800

    Initial skeleton

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Bank-Account-Management-System
+
 Design and implement a Bank Account Management System in C++ using ObjectOriented Programming (OOP) concepts and singly linked lists.
+Your system should define appropriate classes, encapsulating data and behaviors
+using principles such as encapsulation, modularity, and dynamic memory
+management.
---
+++ main.cpp
@@ -0,0 +1,131 @@
+/**********|**********|**********|
+Program: main.cpp
+Course: OOPDS
+Trimester: 2420
+Lecture Class:
+Tutorial Class:
+Trimester: 2430
+Member_1: |  |  |
+Member_2: 242UC244PP | Nicholas Beh Zhi Yang | NICHOLAS.BEH.ZHI@student.mmu.edu.my | 011-65215166
+Member_3: 242UC24551 | LOW ZHENG HAO | LOW.ZHENG.HAO@student.mmu.edu.my | 013-8888444
+**********|**********|**********/
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class BankAccount
+{
+private:
+    int accountNumber;
+    string customerName;
+    double balance;
+
+public:
+    BankAccount(int accNo = 0, string name = " ", double bal = 0.0)
+    {
+        accountNumber = accNo;
+        customerName = name;
+        balance = bal;
+    }
+
+    int getAccountNo() const { return accountNumber; }
+    string getCustomerName() const { return customerName; }
+    double getBalance() const { return balance; }
+
+    void deposit(double amount)
+    {
+        if (amount > 0)
+        {
+            balance += amount;
+            cout << "Deposited: " << amount << " | New Balance: " << balance << endl;
+        }
+        else
+        {
+            cout << "Invalid deposit amount!\n";
+        }
+    }
+
+    bool withdraw(double amount)
+    {
+        if (amount <= 0)
+        {
+            cout << "Invalid withdrawal amount!\n";
+            return false;
+        }
+        if (amount > balance)
+        {
+            cout << "Insufficient balance!\n";
+            return false;
+        }
+        balance -= amount;
+        cout << "Withdrawn: " << amount << " | New Balance: " << balance << endl;
+        return true;
+    }
+
+    void display() const
+    {
+        cout << "Account #" << accountNumber << " | Name: " << customerName << " | Balance: " << balance << endl;
+    }
+};
+
+BankAccount createAccount()
+{
+    int accountNumber;
+    string customerName;
+    double balance;
+
+    cout << "Enter Account Number: ";
+    cin >> accountNumber;
+
+    cin.ignore();
+    cout << "Enter Customer Name: ";
+    getline(cin, customerName);
+
+    cout << "Enter Initial Balance: ";
+    cin >> balance;
+
+    if (balance < 0)
+    {
+        cout << "Balance cannot be negative. Setting to 0.\n";
+        balance = 0;
+    }
+
+    return BankAccount(accountNumber, customerName, balance);
+}
+
+int main()
+{
+    int choice;
+    BankAccount account; // for now just one account (later we use linked list)
+
+    while (true)
+    {
+        cout << " ****************************** " << endl;
+        cout << " ----MENU----" << endl;
+        cout << " 1. Add a new account " << endl;
+        cout << " 2. Display all accounts " << endl;
+        cout << " 3. Deposit money " << endl;
+        cout << " 4. Withdraw money " << endl;
+        cout << " 5. Search account " << endl;
+        cout << " 6. Delete account " << endl;
+        cout << " 7. Exit " << endl;
+        cout << endl;
+        cout << " ****************************** " << endl;
+        cout << endl;
+        cout << " Enter your choice: ";
+
+        cin >> choice;
+
+        switch (choice)
+        {
+        case 1:
+            account = createAccount(); //  actually calls your function
+            cout << "Account created successfully!\n";
+            break;
+        }
+    }
+
+    return 0;
+}
